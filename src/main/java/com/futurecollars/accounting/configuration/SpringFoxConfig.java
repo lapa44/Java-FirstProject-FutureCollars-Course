93e5388dbc960cb349d2508105fd7c8500f8d863
irek <hyperenigmatic@gmail.com>
2020-02-10 20:25:43 +0100

    Swagger - added sample TempController and modified ApiInfo

--- src/main/java/com/futurecollars/accounting/configuration/SpringFoxConfig.java
+++ src/main/java/com/futurecollars/accounting/configuration/SpringFoxConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -16,8 +20,23 @@ public class SpringFoxConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.any())
+        .apis(RequestHandlerSelectors
+            .basePackage("com.futurecollars.accounting"))
         .paths(PathSelectors.any())
-        .build();
+        .build()
+        .consumes(Collections.singleton("application/json"))
+        .produces(Collections.singleton("application/json"))
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfo(
+        "Accounting application.",
+        "The API allows to generate and manage invoices.",
+        "1.0",
+        "Terms of service",
+        new Contact("P-16", "https://futurecollars.com/pl/",
+            "hello@futurecollars.com"),
+        "License of API", "API license URL", Collections.emptyList());
   }
 }
