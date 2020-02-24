339cd5deab3130d05af995a58e6d65550761c196
lapa44 <39618410+lapa44@users.noreply.github.com>
2020-02-24 11:26:24 +0100

    Moved BeforeEach from DatabaseTest to HibernateDatabaseTest.

--- src/test/java/com/futurecollars/accounting/infrastructure/database/HibernateDatabaseTest.java
+++ src/test/java/com/futurecollars/accounting/infrastructure/database/HibernateDatabaseTest.java
@@ -1,6 +1,9 @@
 package com.futurecollars.accounting.infrastructure.database;
 
+import java.util.List;
+import com.futurecollars.accounting.domain.model.Invoice;
 import com.futurecollars.accounting.infrastructure.database.hibernate.InvoiceRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,5 +16,14 @@ class HibernateDatabaseTest extends DatabaseTest {
   @Override
   Database getDatabase() {
     return new HibernateDatabase(invoiceRepository);
+  }
+
+  @BeforeEach
+  public void clearDatabase() throws DatabaseOperationException {
+    Database database = getDatabase();
+    List<Invoice> ls = database.getInvoices();
+    for (Invoice inv : ls) {
+      database.removeInvoiceById(inv.getId());
+    }
   }
 }
